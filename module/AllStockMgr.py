ac97f0905ebf27d7792df724df74eb46cf9bbc03
kangyangshih <dandanshih1980@gmail.com>
2020-12-03 22:47:57 +0800

    update more information in dailyreport

--- module/AllStockMgr.py
+++ module/AllStockMgr.py
@@ -6,6 +6,8 @@ sys.path.append(r"c:\download\ranb_gametowner\python_module")
 from utility import *
 from excel_utility import *
 import json
+sys.path.append (r"..\module")
+from NetStockInfo import NetStockInfo
 
 # 單一股票
 class cSingleStock :
@@ -35,7 +37,8 @@ class cSingleStock :
         # 其他描述
         self.desc = ""
         self.netInfo = {}
-
+    
+    # 取得字串資訊
     def getInfo (self, *args):
         #print (args)
         # 取得直接參數
@@ -50,19 +53,175 @@ class cSingleStock :
             target = target[args[index]]
         return target
     
+    # 取得資料, 轉成 INT
     def getInfoInt (self, *args):
         res = self.getInfo (*args)
         if res == None:
             return None
         return int(res.replace (",", ""))
-    
+
+    # 取得資料, 轉成浮點數    
     def getInfoFloat (self, *args):
         res = self.getInfo (*args)
         if res == None:
             return None
         return float (res.replace (",", ""))
 
+    # 做寫入的動作
+    def _write (self, file, res, strFormat, *args):
+        # 串接字串
+        strtmp = (strFormat+"\n") % args
+        # 印到畫面上
+        print (strtmp, end="")
+        # 如果有需要寫檔就做寫入的動作
+        if file != None:
+            file.writelines (strtmp)
+        # 把字串存起來
+        res.append (strtmp)
+
+    # 寫入單股資料
+    def dumpInfo (self, file=None):
+        res = []
+        #------------------------
+        # 寫入基本資料
+        self._write (file, res, "#-------------------------------")
+        self._write (file, res, "# %s(%s) 股本 %.2f 億", self.id, self.name, self.getInfoFloat ("股本"))
+        self._write (file, res, "#-------------------------------")
+        #------------------------
+        # 今天的漲跌幅
+        realtime = NetStockInfo.getYahooRealtime (self.id, False)
+        #print (realtime)
+        self._write (file, res, "[本日股價表現]")
+        self._write (file, res, "%s %.1f 量 : %s", realtime["now_price"], realtime["now_result"], realtime["now_vol"])
+        self._write (file, res, "")
+
+        #------------------------
+        # 可以加入自己的評價
+        self._write (file, res, "[本日簡評]")
+        # 2020 預估 EPS
+        eps2020 = 0
+        if self.getInfo ("QEPS", "2020Q3", "EPS") != None and self.getInfo ("QEPS", "2020Q2", "EPS") != None and self.getInfo ("QEPS", "2020Q1", "EPS") != None:
+            eps2020 = self.getInfoFloat ("QEPS", "2020Q3", "EPS") * 2 \
+                    + self.getInfoFloat ("QEPS", "2020Q2", "EPS") \
+                    + self.getInfoFloat ("QEPS", "2020Q1", "EPS")
+            self._write (file, res, "2020 預估全年 EPS : %.2f", eps2020)
+        else:
+            self._write (file, res, "無法預估2020 EPS")
+
+        # 2021 預估配股配息和目前殖利率
+        #print (self._getStockDividenRate())
+        sd2021_money = eps2020 * self._getStockDividenRate() / 100
+        self._write (file, res, "2021 預估配息 : %.2f 配息率 : %.2f %%", sd2021_money, self._getStockDividenRate())
+        self._write (file, res, "目前殖利率預估 : %.2f %%", sd2021_money / realtime["now_price"] * 100 )
+
+        # 結束
+        self._write (file, res, "")
+
+        #------------------------
+        # 最近五天3大法人動作
+        self._write (file, res, "[近日三大法人動向]")
+        for index in range (5):
+            #self._write (file, res, "%s", json.dumps (self.getInfo ("三大法人")[index]))
+            self._write (file, res, "%s 外資:%s, 投信:%s", 
+                self.getInfo ("三大法人")[index]["date"], 
+                self.getInfo ("三大法人")[index]["out"], 
+                self.getInfo ("三大法人")[index]["in"],
+            )
+        self._write (file, res, "")
+
+        #------------------------
+        # 近三個月的營收
+        self._write (file, res, "[最近月營收]")
+        self._write (file, res, "%s 月增: %s %%, 年增: %s %%, 累計年增: %s %%", 
+            self.getInfo ("月營收", "2020/10", "年度/月份"),
+            self.getInfo ("月營收", "2020/10", "月增"),
+            self.getInfo ("月營收", "2020/10", "年増"),
+            self.getInfo ("月營收", "2020/10", "累計年增"),
+        )
+        self._write (file, res, "%s 月增: %s %%, 年增: %s %%, 累計年增: %s %%", 
+            self.getInfo ("月營收", "2020/09", "年度/月份"),
+            self.getInfo ("月營收", "2020/09", "月增"),
+            self.getInfo ("月營收", "2020/09", "年増"),
+            self.getInfo ("月營收", "2020/09", "累計年增"),
+        )
+        self._write (file, res, "%s 月增: %s %%, 年增: %s %%, 累計年增: %s %%", 
+            self.getInfo ("月營收", "2020/08", "年度/月份"),
+            self.getInfo ("月營收", "2020/08", "月增"),
+            self.getInfo ("月營收", "2020/08", "年増"),
+            self.getInfo ("月營收", "2020/08", "累計年增"),
+        )
+        self._write (file, res, "")
+
+        #------------------------
+        # 前三季 EPS
+        self._write (file, res, "[2020前三季EPS]")
+        self._write (file, res, 
+            "2020Q3 EPS:%s, 毛利率 : %s %%, 營業利益率 : %s %%, 稅前淨利率:%s %%",
+            self.getInfo ("QEPS", "2020Q3", "EPS"),
+            self.getInfo ("QEPS", "2020Q3", "毛利率"),
+            self.getInfo ("QEPS", "2020Q3", "營業利益率"),
+            self.getInfo ("QEPS", "2020Q3", "稅前淨利率"),
+        )
+        self._write (file, res, 
+            "2020Q2 EPS:%s, 毛利率 : %s %%, 營業利益率 : %s %%, 稅前淨利率:%s %%",
+            self.getInfo ("QEPS", "2020Q2", "EPS"),
+            self.getInfo ("QEPS", "2020Q2", "毛利率"),
+            self.getInfo ("QEPS", "2020Q2", "營業利益率"),
+            self.getInfo ("QEPS", "2020Q2", "稅前淨利率"),
+        )
+        self._write (file, res, 
+            "2020Q1 EPS:%s, 毛利率 : %s %%, 營業利益率 : %s %%, 稅前淨利率:%s %%",
+            self.getInfo ("QEPS", "2020Q1", "EPS"),
+            self.getInfo ("QEPS", "2020Q1", "毛利率"),
+            self.getInfo ("QEPS", "2020Q1", "營業利益率"),
+            self.getInfo ("QEPS", "2020Q1", "稅前淨利率"),
+        )
+
+        #------------------------
+        # 去年EPS
+        self._write (file, res, "2019 EPS : %s 元", self.getInfo ("QEPS", "2019", "EPS"))
+        self._write (file, res, "")
+
+        #------------------------
+        # 近幾年的配股配息
+        self._write (file, res, "[配股配息]")
+        sdList = self.getInfo ("配股配息")
+        for index in range (5):
+            # 不滿五年就不印了
+            if index >= len(sdList):
+                break
+            self._write (file, res, 
+                "%s EPS:%s, 股票股利:%s, 現金股利:%s, 配息率:%s",
+                sdList[index]["所屬年度"],
+                sdList[index]["EPS"],
+                sdList[index]["股票股利"],
+                sdList[index]["現金股利"],
+                sdList[index]["配息率"],
+            )
+        self._write (file, res, "")
+
+        #------------------------
+        # 回傳結果
+        return res
     
+    def _getStockDividenRate (self):
+        sdList = self.getInfo ("配股配息")
+        sdRate = 0
+        for index in range (5):
+            # 不滿五年就不印了
+            if index >= len(sdList):
+                return sdRate / (index-1)
+            tmp = 0
+            if sdList[index]["配息率"] != "-":
+                tmp = float (sdList[index]["配息率"].replace ('%', ""))
+            #mp = float (sdList[index]["配息率"].replace ('%', ""))
+            if tmp > 100:
+                tmp = 100
+            if tmp < 0:
+                tmp = 0
+            sdRate += tmp
+        return sdRate / 5
+
 # 股票管理器
 class cAllStockMgr:
     def __loadJsonFromFile (self, filename):
--- tools/stockDailyReport.py
+++ tools/stockDailyReport.py
@@ -24,12 +24,17 @@ def saveCache (stockID, info):
     file.writelines (json.dumps (info))
     file.close()
 
+# 清除暫存檔
+#del_dir ("cache")
+#check_dir ("cache")
+
 # 取得所有的股票
 allstock = AllStockMgr.getAllStock ()
 priorityKey = [
     "持有", 
     "核心", 
-    "台積電概念股", 
+    "台積電", 
+    "聯發科",
     "被動元件", 
     "散熱", 
     "PCB", 
@@ -54,10 +59,6 @@ for stockID, stock in allstock.items():
             value[stockID] = stock
             break
     
-# 清除暫存檔
-#del_dir ("cache")
-#check_dir ("cache")
-
 file = open("../daily.txt", "w", encoding="utf-8")
 def write (strFormat, *args):
     strtmp = (strFormat+"\n") % args
@@ -71,83 +72,13 @@ for key in priorityKey:
     write ("#-------------------------------")
     # 一隻一隻去抓資料處理
     for stockID, stock in stockOrder[key].items():
-        #------------------------
-        # 寫入基本資料
-        write ("#-------------------------------")
-        write ("# %s(%s) 股本 %.2f 億", stockID, stock.name, stock.getInfoFloat ("股本"))
-        write ("#-------------------------------")
-        #------------------------
-        # 今天的漲跌幅
-        realtime = NetStockInfo.getYahooRealtime (stockID, False)
-        #print (realtime)
-        write ("[本日股價表現]")
-        write ("%s %.1f %s", realtime["now_price"], realtime["now_result"], realtime["now_vol"])
-        write ("")
 
-        #------------------------
-        # 最近五天3大法人動作
-        write ("[近日三大法人動向]")
-        for index in range (5):
-            #write ("%s", json.dumps (stock.getInfo ("三大法人")[index]))
-            write ("%s 外資:%s, 投信:%s", 
-                stock.getInfo ("三大法人")[index]["date"], 
-                stock.getInfo ("三大法人")[index]["out"], 
-                stock.getInfo ("三大法人")[index]["in"],
-            )
-        write ("")
+        # 把資料 dump 下來
+        stock.dumpInfo (file)
+        #print (res)
 
-        #------------------------
-        # 近三個月的營收
-        write ("[最近月營收]")
-        write ("%s 月增: %s %%, 年增: %s %%, 累計年增: %s %%", 
-            stock.getInfo ("月營收", "2020/10", "年度/月份"),
-            stock.getInfo ("月營收", "2020/10", "月增"),
-            stock.getInfo ("月營收", "2020/10", "年増"),
-            stock.getInfo ("月營收", "2020/10", "累計年增"),
-        )
-        write ("%s 月增: %s %%, 年增: %s %%, 累計年增: %s %%", 
-            stock.getInfo ("月營收", "2020/09", "年度/月份"),
-            stock.getInfo ("月營收", "2020/09", "月增"),
-            stock.getInfo ("月營收", "2020/09", "年増"),
-            stock.getInfo ("月營收", "2020/09", "累計年增"),
-        )
-        write ("%s 月增: %s %%, 年增: %s %%, 累計年增: %s %%", 
-            stock.getInfo ("月營收", "2020/08", "年度/月份"),
-            stock.getInfo ("月營收", "2020/08", "月增"),
-            stock.getInfo ("月營收", "2020/08", "年増"),
-            stock.getInfo ("月營收", "2020/08", "累計年增"),
-        )
-        write ("")
-
-        #------------------------
-        # 前三季 EPS
-        write ("[2020前三季EPS]")
-        write ("2020Q3 EPS:%s, 毛利率 : %s %%, 營業利益率 : %s %%, 稅前淨利率:%s %%",
-            stock.getInfo ("QEPS", "2020Q3", "EPS"),
-            stock.getInfo ("QEPS", "2020Q3", "毛利率"),
-            stock.getInfo ("QEPS", "2020Q3", "營業利益率"),
-            stock.getInfo ("QEPS", "2020Q3", "稅前淨利率"),
-        )
-        write ("2020Q2 EPS:%s, 毛利率 : %s %%, 營業利益率 : %s %%, 稅前淨利率:%s %%",
-            stock.getInfo ("QEPS", "2020Q2", "EPS"),
-            stock.getInfo ("QEPS", "2020Q2", "毛利率"),
-            stock.getInfo ("QEPS", "2020Q2", "營業利益率"),
-            stock.getInfo ("QEPS", "2020Q2", "稅前淨利率"),
-        )
-        write ("2020Q1 EPS:%s, 毛利率 : %s %%, 營業利益率 : %s %%, 稅前淨利率:%s %%",
-            stock.getInfo ("QEPS", "2020Q1", "EPS"),
-            stock.getInfo ("QEPS", "2020Q1", "毛利率"),
-            stock.getInfo ("QEPS", "2020Q1", "營業利益率"),
-            stock.getInfo ("QEPS", "2020Q1", "稅前淨利率"),
-        )
-
-        #------------------------
-        # 去年EPS
-        write ("2019 EPS : %s 元", stock.getInfo ("QEPS", "2019", "EPS"))
-        write ("")
-
-        #------------------------
-
+        # 暫時只先抓一隻
+        #break
     # 暫時只抓持有的
     #break
 
