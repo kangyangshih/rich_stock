036bcf119fff333fd69303f9da37a9eb54689012
kangyangshih <dandanshih1980@gmail.com>
2020-12-10 18:19:53 +0800

    更新 12/10 資料

--- tools/updateStockInfo.py
+++ tools/updateStockInfo.py
@@ -29,7 +29,7 @@ def saveCache (stockID, info):
 allstock = AllStockMgr.getAllStock ()
 miss_qeps = 0
 miss_turnover = 0
-threeKey = "2020/12/09"
+threeKey = "2020/12/10"
 
 epsKey = "2020Q3"
 turnOverKey = "2020/10"
