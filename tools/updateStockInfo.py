b69c6d82181d177e5165515f74fcb7db174a88d8
kangyangshih <dandanshih1980@gmail.com>
2020-12-25 19:12:45 +0800

    update 12/25

--- tools/updateStockInfo.py
+++ tools/updateStockInfo.py
@@ -44,7 +44,7 @@ def saveContinueCache (stockID, info):
 allstock = AllStockMgr.getAllStock ()
 miss_qeps = 0
 miss_turnover = 0
-threeKey = "2020/12/24"
+threeKey = "2020/12/25"
 
 epsKey = "2020Q3"
 turnOverKey = "2020/11"
