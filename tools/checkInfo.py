5757db39da502e3ad7196fabb3d5237434cfc366
kangyangshih <dandanshih1980@gmail.com>
2021-01-29 16:08:29 +0800

    【更新】新增移除 EXCEL 刪除後不需要的檔案

---
+++ tools/checkInfo.py
@@ -0,0 +1,35 @@
+# 做一些 info 的資料更新
+# TODO
+# 把三大法人拔出來
+# 每日資料拔出來
+
+import sys
+sys.path.append(r"c:\download\ranb_gametowner\python_module")
+from utility import *
+#WebViewMgr.debugMode ()
+# 載入這個專案共用模組
+sys.path.append (r"..\module")
+from AllStockMgr import AllStockMgr
+from NetStockInfo import NetStockInfo
+import json
+import csv
+
+# 取得所有的股票
+allstock = AllStockMgr.getAllStock ()
+
+#-----------------------------
+# 處理被移除掉的資料
+#-----------------------------
+print ("== 處理被移除 excel 的檔案 ==")
+# 取得目錄下的檔案
+dirFileList = get_dir_file_list ("../info")
+
+# 取得被刪除的檔案，做刪除的動作
+for stockID, stock in allstock.items():
+    tmp = "../info/%s.txt" % (stockID,)
+    if tmp in dirFileList:
+        dirFileList.remove (tmp)
+
+print ("需要被移除的列表:", dirFileList)
+for removeFile in dirFileList:
+    del_file (removeFile)
